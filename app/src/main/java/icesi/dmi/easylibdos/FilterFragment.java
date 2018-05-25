3f3dea141dc51b066f6c21e25e30e869c8574471
Juan Pablo <jpgarias09@gmail.com>
2018-05-25 06:45:46 -0500

    Different menus working with frame Layout

--- app/src/main/java/icesi/dmi/easylibdos/FilterFragment.java
+++ app/src/main/java/icesi/dmi/easylibdos/FilterFragment.java
@@ -4,6 +4,7 @@ package icesi.dmi.easylibdos;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.InflateException;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -30,7 +31,10 @@ public class FilterFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
+        //Goone
         return inflater.inflate(R.layout.fragment_filter, container, false);
+
     }
 
 }
